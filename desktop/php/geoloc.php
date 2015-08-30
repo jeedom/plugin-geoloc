c44801a58db26485ac9a31208fd39664e3b632a3
Loic <loicgevrey@gmail.com>
2015-08-30 17:15:52 +0200

    UI improvement

--- desktop/php/geoloc.php
+++ desktop/php/geoloc.php
@@ -13,7 +13,8 @@ $eqLogics = eqLogic::byType('geoloc');
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
 foreach ($eqLogics as $eqLogic) {
-	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"  style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
            </ul>
@@ -32,10 +33,7 @@ foreach ($eqLogics as $eqLogic) {
 </div>
 <?php
 foreach ($eqLogics as $eqLogic) {
-	$opacity = '';
-	if ($eqLogic->getIsEnable() != 1) {
-		$opacity = 'opacity:0.3;';
-	}
+	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 	echo "<center>";
 	echo '<img src="plugins/geoloc/doc/images/geoloc_icon.png" height="105" width="95" />';
