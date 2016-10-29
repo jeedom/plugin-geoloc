6a3f56a2559f259a3bb938993b0fa0b9103413e2
bbreton09 <bbreton@gmail.com>
2016-10-29 22:52:16 +0200

    Update bootstrapSwitch forget <label>

--- desktop/js/geoloc.js
+++ desktop/js/geoloc.js
@@ -107,12 +107,13 @@ function addCmdToTable(_cmd) {
     tr += '</select>';
     tr += '</span>';
     tr += '<span class="travelTime travelDistance modeOption" style="display : none;"> ';
-    tr += '<input type="checkbox" class="cmdAttr input-sm" data-l1key="configuration" data-l2key="noHighways" style="position:relative;top:10px;" data-size="small" data-label-text="{{Autoroutes}}"> ';
+    tr += '<label class="checkbox"><input type="checkbox" class="cmdAttr input-sm" data-l1key="configuration" data-l2key="noHighways" style="position:relative;top:10px;" data-size="small" checked/>{{Autoroutes}}</label> ';
     tr += '</span>';
     tr += '</td>';
     tr += '<td>';
-    tr += '<span class="modeOption distance" style="display:none;"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized"  data-size="mini" data-label-text="{{Historiser}}" /></span> ';
-    tr += '<span><input type="checkbox" class="cmdAttr" data-l1key="isVisible" data-size="mini" data-label-text="{{Afficher}}" checked/></span> ';
+    tr += '<span class="modeOption distance" style="display : none;"> ';
+    tr += '<label class="checkbox"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized" data-size="mini" checked/>{{Historiser}}</label></span> ';
+    tr += '<span><label class="checkbox"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" data-size="mini" checked/>{{Afficher}}</label></span> ';
     tr += '</td>';
     tr += '<td>';
     if (is_numeric(_cmd.id)) {
