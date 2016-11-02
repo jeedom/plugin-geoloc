e3616eaccb56ec54a45a33ac889bd8dc00b45507
Ethal <developer@ethal-consulting.com>
2016-11-02 08:54:12 +0100

    update

--- desktop/php/geoloc.php
+++ desktop/php/geoloc.php
@@ -100,10 +100,9 @@ foreach (object::all() as $object) {
     </div>
 
 <div role="tabpanel" class="tab-pane" id="commandtab">	
-	<legend><i class="fa fa-list-alt"></i>  {{Tableau de commandes}}</legend>
 	<div class="alert alert-info">Exemple d’URL à appeler avec tasker : <?php echo network::getNetworkAccess('external')?>/core/api/jeeApi.php?api=<?php echo config::byKey('api');?>&type=geoloc&id=#ID_CMD#&value=%LOCN</div>
 	<a class="btn btn-success btn-sm pull-right cmdAction" data-action="add"><i class="fa fa-plus-circle"></i> {{Commandes}}</a>
-	<a class="control-label pull-right" href="http://www.coordonnees-gps.fr/" target="_blank"><i class="icon nature-planet5"></i> Cliquez-ici pour retrouver vos coordonnées</a><br/><br/>
+	<a class="control-label pull-left" href="http://www.coordonnees-gps.fr/" target="_blank"><i class="icon nature-planet5"></i>{{Cliquez-ici pour retrouver vos coordonnées}}</a><br/><br/>
 	<table id="table_cmd" class="table table-bordered table-condensed">
     	<thead>
         	<tr>
