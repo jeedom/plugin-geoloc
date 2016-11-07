c822bcd55a4a089542a56958b71772aa2171b90f
Ethal <developer@ethal-consulting.com>
2016-11-07 08:30:44 +0100

    Update geoloc.php

--- desktop/php/geoloc.php
+++ desktop/php/geoloc.php
@@ -51,7 +51,7 @@ foreach ($eqLogics as $eqLogic) {
 	<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
 	<ul class="nav nav-tabs" role="tablist">
 		<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
-		<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer active"></i> {{Equipement}}</a></li>
+		<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
 		<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
 	</ul>
 
